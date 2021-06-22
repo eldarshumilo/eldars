36a5f49f19ac293b0f004668039f287f9ef01676
Eldar <74300376+eldarshumilo@users.noreply.github.com>
2021-06-22 09:43:05 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -161,7 +161,7 @@ const video = document.querySelector("#pose-video");
     if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
       document.getElementsByTagName('meta')[1].setAttribute( 'content', 'width=device-width,initial-scale=1');
       if(ratio === 1.78){
-        document.getElementsByTagName('meta')[1].setAttribute( 'content', 'width=device-width,initial-scale=0.2');
+        document.getElementsByTagName('meta')[1].setAttribute( 'content', 'width=device-width,initial-scale=0.4');
         canvas.style.top = '15%';
       } else if ( ratio == 2){
         canvas.style.top = '21%';
