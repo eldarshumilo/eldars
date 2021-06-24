ad0764c9abea3462d6c1bf8c178b33c14114f35a
Eldar <74300376+eldarshumilo@users.noreply.github.com>
2021-06-24 18:25:59 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,181 +1,211 @@
 const video = document.querySelector("#pose-video");
-  const widthVideo = video.offsetWidth;
-  const heightVideo = video.offsetHeight; 
-  const config = {
-    video: { width: widthVideo, height: heightVideo, fps: 30 }
-  };
-  const res = document.querySelector(".result");
-  res.innerHTML = "Initialization1";
+const widthVideo = video.offsetWidth;
+const heightVideo = video.offsetHeight;
+const config = {
+  video: {
+    width: widthVideo,
+    height: heightVideo,
+    fps: 30
+  }
+};
+const res = document.querySelector(".result");
+res.innerHTML = "Initialization1";
 
-  const landmarkColors = {
-    thumb: 'red',
-    indexFinger: 'blue',
-    middleFinger: 'yellow',
-    ringFinger: 'green',
-    pinky: 'pink',
-    palmBase: 'white'
-    
-  };
+const landmarkColors = {
+  thumb: 'red',
+  indexFinger: 'blue',
+  middleFinger: 'yellow',
+  ringFinger: 'green',
+  pinky: 'pink',
+  palmBase: 'white'
 
-  const redbox = document.querySelector('.redbox'); 
-  const bluebox = document.querySelector('.bluebox'); 
-  function Box (boxOn, boxOff){
-    boxOn.style.display = 'block'; 
-    boxOff.style.display = 'none'; 
-    clearTimeout(off);
-    var off = setTimeout(()=>{boxOn.style.display = 'none'; res.innerHTML='';}, 2000)
-    
-  };
+};
 
-  res.innerHTML = "Initialization2";
-  async function main() {
-    var canvasDraw = document.getElementById("draw-canvas"), 
+const redbox = document.querySelector('.redbox');
+const bluebox = document.querySelector('.bluebox');
+
+function Box(boxOn, boxOff) {
+  boxOn.style.display = 'block';
+  boxOff.style.display = 'none';
+
+  clearTimeout(off);
+  var off = setTimeout(() => {
+    boxOn.style.display = 'none';
+    res.innerHTML = '';
+  }, 2000)
+
+};
+
+res.innerHTML = "Initialization2";
+async function main() {
+  var canvasDraw = document.getElementById("draw-canvas"),
     context = canvasDraw.getContext("2d");
-    canvasDraw.height = config.video.height;
-    canvasDraw.width = config.video.width;
-    context.beginPath();
+  canvasDraw.height = config.video.height;
+  canvasDraw.width = config.video.width;
+  //context.beginPath();
+
+
+
+  const canvas = document.querySelector("#pose-canvas");
+  const ctx = canvas.getContext("2d");
+
+  const knownGestures = [
+    fp.Gestures.VictoryGesture,
+    fp.Gestures.ThumbsUpGesture,
+    fp.Gestures.IndexUp
+  ];
+  const GE = new fp.GestureEstimator(knownGestures);
+  redbox.style.display = 'none'
+  bluebox.style.display = 'none'
+
+  // load handpose model
+  res.innerHTML = "Initialization3";
+  const model = await handpose.load();
+  res.innerHTML = "Initialization done";
+  console.log("Handpose model loaded");
+
+
+  function clear(cds, width, height) {
+    cds.beginPath();
+    cds.clearRect(0, 0, width, height);
+    cds.closePath();
+  }
+  // main estimation loop
+  const estimateHands = async () => {
+
+    // clear canvas overlay
+    ctx.clearRect(0, 0, config.video.width, config.video.height);
+
     
-    const canvas = document.querySelector("#pose-canvas");
-    const ctx = canvas.getContext("2d");
-    
-    const knownGestures = [
-      fp.Gestures.VictoryGesture,
-      fp.Gestures.ThumbsUpGesture,
-      fp.Gestures.IndexUp
-    ];
-    const GE = new fp.GestureEstimator(knownGestures);
-    redbox.style.display = 'none'
-    bluebox.style.display = 'none'
 
-    // load handpose model
-    res.innerHTML = "Initialization3";
-    const model = await handpose.load();
-    res.innerHTML = "Initialization done";
-    console.log("Handpose model loaded");
+    // get hand landmarks from video
+    // Note: Handpose currently only detects one hand at a time
+    // Therefore the maximum number of predictions is 1
+    const predictions = await model.estimateHands(video, true);
 
-    // main estimation loop
-    const estimateHands = async () => {
+    for (let i = 0; i < predictions.length; i++) {
 
-      // clear canvas overlay
-      ctx.clearRect(0, 0, config.video.width, config.video.height);
-    
-      // get hand landmarks from video
-      // Note: Handpose currently only detects one hand at a time
-      // Therefore the maximum number of predictions is 1
-      const predictions = await model.estimateHands(video, true);
 
-      for(let i = 0; i < predictions.length; i++) {
-
-        
-        // now estimate gestures based on landmarks
-        // using a minimum confidence of 7.5 (out of 10)
-        function resul(){
+      // now estimate gestures based on landmarks
+      // using a minimum confidence of 7.5 (out of 10)
+      function resul() {
         const est = GE.estimate(predictions[i].landmarks, 7.5);
-        
-        if(est.gestures.length > 0) {
+
+        if (est.gestures.length > 0) {
 
           // find gesture with highest confidence
-          let result = est.gestures.reduce((p, c) => { 
+          let result = est.gestures.reduce((p, c) => {
             return (p.confidence > c.confidence) ? p : c;
           });
-          
-          function addEvent() { 
-            if (result.name == "thumbs_up"){
+
+          function addEvent() {
+            if (result.name == "thumbs_up") {
               Box(redbox, bluebox);
               res.innerHTML = "<= It's 'Like'";
-            } else if (result.name == "victory"){
+            } else if (result.name == "victory") {
               Box(bluebox, redbox);
+
               res.innerHTML = "It's 'Peace' =>";
-            } else if (result.name == "indexUp"){
+            } else if (result.name == "indexUp") {
               let w = Math.round(predictions[i].annotations.indexFinger[3][0]);
               let h = Math.round(predictions[i].annotations.indexFinger[3][1]);
               res.innerHTML = `x: ${w} y: ${h}`;
-            }        
+            }
           }
           addEvent();
-          return result.name;
+          const dot = result
+          return result.name
         }
       }
-      resul();
-      const dot = resul();
-      // draw colored dots at each predicted joint position
+      //resul();
+      const dot =  resul();
+      if (dot=="victory"){
+        clear(context, config.video.width, config.video.height);
+      }
 
-      if(dot !== 'indexUp'){
-        
-        drawSmth(context, predictions[i].annotations.indexFinger[3][0], predictions[i].annotations.indexFinger[3][1], 1);
-        context.moveTo(predictions[0].annotations.indexFinger[3][0], predictions[0].annotations.indexFinger[3][1]);
-        for(let part in predictions[i].annotations) {
-          for(let point of predictions[i].annotations[part]) {
-            drawPoint(ctx, point[0], point[1], 10, landmarkColors[part]);
-          }
-        } 
+      drawSmth(context, predictions[i].annotations.indexFinger[3][0], predictions[i].annotations.indexFinger[3][1], 1);
+      context.moveTo(predictions[0].annotations.indexFinger[3][0], predictions[0].annotations.indexFinger[3][1]);
+      for (let part in predictions[i].annotations) {
+        for (let point of predictions[i].annotations[part]) {
+          drawPoint(ctx, point[0], point[1], 10, landmarkColors[part]);
+        }
+      }
+      
+      // draw colored dots at each predicted joint position 
+    }
+    // ...and so on
+    setTimeout(() => {
+      estimateHands();
+    }, 1000 / config.video.fps);
+    
+  };
+
+  estimateHands();
+
+
+  console.log("Starting predictions");
+
+}
+
+async function initCamera(width, height, fps) {
+
+  const constraints = {
+    audio: false,
+    video: {
+      facingMode: "user",
+      width: width,
+      height: height,
+      frameRate: {
+        max: fps
       }
     }
-      // ...and so on
-      setTimeout(() => { estimateHands(); }, 1000 / config.video.fps);
+  };
+
+  const video = document.querySelector("#pose-video");
+  video.width = width;
+  video.height = height;
+
+  // get video stream
+  const stream = await navigator.mediaDevices.getUserMedia(constraints);
+  video.srcObject = stream;
+
+  return new Promise(resolve => {
+    video.onloadedmetadata = () => {
+      resolve(video)
     };
-   
-    estimateHands();
-    console.log("Starting predictions");
-  }
-
-  async function initCamera(width, height, fps) {
-
-    const constraints = {
-      audio: false,
-      video: {
-        facingMode: "user",
-        width: width,
-        height: height,
-        frameRate: { max: fps }
-      }
-    };
-
-    const video = document.querySelector("#pose-video");
-    video.width = width;
-    video.height = height;
-
-    // get video stream
-    const stream = await navigator.mediaDevices.getUserMedia(constraints);
-    video.srcObject = stream;
-
-    return new Promise(resolve => {
-      video.onloadedmetadata = () => { resolve(video) };
-    });
-  }
-
-
-  function drawSmth(context, x, y) {  
-        context.lineTo(x, y);
-        context.stroke();
-        console.log(x, y);
-        
-      }
-     
-
-  function drawPoint(ctx, x, y, r, color) {
-    ctx.beginPath();
-    ctx.arc(x, y, r, 0, 2 * Math.PI);
-    ctx.fillStyle = color;
-    ctx.fill();
-  }
-
-  window.addEventListener("DOMContentLoaded", () => {
-    initCamera(
-      config.video.width, config.video.height, config.video.fps
-    ).then(video => {
-      video.addEventListener("loadeddata", event => {
-        console.log("Camera is ready");
-        main();
-      });
-    });
-
-
-    const canvas = document.querySelector("#pose-canvas");
-
-      canvas.width = config.video.width;
-      canvas.height = config.video.height;
-
-    
- 
   });
+}
+
+
+function drawSmth(context, x, y) {
+  //context.beginPath();
+  context.lineTo(x, y);
+  context.stroke();
+  //context.closePath();
+  console.log(x, y);
+}
+
+
+function drawPoint(ctx, x, y, r, color) {
+  ctx.beginPath();
+  ctx.arc(x, y, r, 0, 2 * Math.PI);
+  ctx.fillStyle = color;
+  ctx.fill();
+  ctx.closePath();
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  initCamera(
+    config.video.width, config.video.height, config.video.fps
+  ).then(video => {
+    video.addEventListener("loadeddata", event => {
+      console.log("Camera is ready");
+      main();
+    });
+  });
+
+
+  const canvas = document.querySelector("#pose-canvas");
+  canvas.width = config.video.width;
+  canvas.height = config.video.height;
+});
